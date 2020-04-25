f6df2138a13102c350db9c06c0a13264e1f66906
Caleb <caleb@fire.coop>
2020-04-24 17:07:18 -0700

    try to fix error

--- app/decorators/controllers/solidus_multi_domain/spree/api/shipments_controller_decorator.rb
+++ app/decorators/controllers/solidus_multi_domain/spree/api/shipments_controller_decorator.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
+module SolidusMultiDomain
+  module Spree
+    module Api
+      module ShipmentsControllerDecorator
+        def self.prepended(base)
+          base.prepend SolidusMultiDomain::CreateLineItemSupport
+        end
 
-module SpreeMultiStore
-  module Api
-    module ShipmentsControllerDecorator
-      def self.prepended(base)
-        base.prepend SolidusMultiDomain::CreateLineItemSupport
+        def mine
+          super
+          @shipments = @shipments.where(spree_orders: { store_id: current_store.id }) if @shipments
+        end
+
+        ::Spree::Api::ShipmentsController.prepend(self) if SolidusMultiDomain::Engine.api_available?
       end
-
-      def mine
-        super
-        @shipments = @shipments.where(spree_orders: { store_id: current_store.id }) if @shipments
-      end
-
-      ::Spree::Api::ShipmentsController.prepend(self) if SolidusMultiDomain::Engine.api_available?
     end
   end
 end
